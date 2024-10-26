d54ecfe2e462c944a5f0459474a7ef36fbb3823d
Tawfiqur Rahman <tawfiqur@brainstation-23.com>
2024-10-26 12:01:58 +0600

    Increase build number

--- common/common-utils/src/main/java/com/duckduckgo/common/utils/Constants.kt
+++ common/common-utils/src/main/java/com/duckduckgo/common/utils/Constants.kt
@@ -24,6 +24,7 @@ const val SAFE_GAZE_INTERFACE = "SafeGazeInterface"
 const val SAFE_GAZE_PREFERENCES = "safe_gaze_preferences"
 const val SAFE_GAZE_DEFAULT_BLUR_VALUE = 50
 const val SAFE_GAZE_MIN_FACE_SIZE = 15
+const val SAFE_GAZE_MIN_IMG_SIZE = 45
 const val KAHF_GUARD_BLOCKED_URL = "blocked.kahfguard.com"
 
 // Private DNS Settings
