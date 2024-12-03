93f89b1d3ce833a62a91fe81ef79b13c55433472
alganzory <md.alganzory@gmail.com>
2024-12-03 21:08:04 +0400

    bug fixes: resize large images, graceful error handling

--- common/common-utils/src/main/java/com/duckduckgo/common/utils/Constants.kt
+++ common/common-utils/src/main/java/com/duckduckgo/common/utils/Constants.kt
@@ -25,6 +25,7 @@ const val SAFE_GAZE_PREFERENCES = "safe_gaze_preferences"
 const val SAFE_GAZE_DEFAULT_BLUR_VALUE = 50
 const val SAFE_GAZE_MIN_FACE_SIZE = 15
 const val SAFE_GAZE_MIN_IMG_SIZE = 45
+const val SAFE_GAZE_MAX_IMG_SIZE= 800
 const val KAHF_GUARD_BLOCKED_URL = "blocked.kahfguard.com"
 
 // Private DNS Settings
