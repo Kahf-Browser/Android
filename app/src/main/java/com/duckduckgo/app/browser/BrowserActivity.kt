9cd86eecf6dffe110457e64763b7a1de10dbc1e7
abdullah_mahmood <miq0717@gmail.com>
2025-07-22 11:35:09 +0600

    add: Log added to check if the browser is still default browser in user's device

--- app/src/main/java/com/duckduckgo/app/browser/BrowserActivity.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserActivity.kt
@@ -38,6 +38,9 @@ import androidx.webkit.ServiceWorkerClientCompat
 import androidx.webkit.ServiceWorkerControllerCompat
 import androidx.webkit.WebViewFeature
 import com.duckduckgo.anvil.annotations.InjectWith
+import com.duckduckgo.app.Days
+import com.duckduckgo.app.analytics.AnalyticsEvent
+import com.duckduckgo.app.analytics.AnalyticsService
 import com.duckduckgo.app.browser.BrowserViewModel.Command
 import com.duckduckgo.app.browser.BrowserViewModel.Command.Query
 import com.duckduckgo.app.browser.databinding.ActivityBrowserBinding
@@ -56,6 +59,7 @@ import com.duckduckgo.app.global.sanitize
 import com.duckduckgo.app.global.view.ClearDataAction
 import com.duckduckgo.app.global.view.FireDialog
 import com.duckduckgo.app.global.view.renderIfChanged
+import com.duckduckgo.app.isMyBrowserDefault
 import com.duckduckgo.app.onboarding.ui.page.DefaultBrowserPage
 import com.duckduckgo.app.pixels.AppPixelName
 import com.duckduckgo.app.pixels.AppPixelName.FIRE_DIALOG_CANCEL
@@ -74,6 +78,7 @@ import com.duckduckgo.common.ui.view.show
 import com.duckduckgo.common.ui.viewbinding.viewBinding
 import com.duckduckgo.common.utils.AD_REFRESH_INTERVAL
 import com.duckduckgo.common.utils.DispatcherProvider
+import com.duckduckgo.common.utils.LAST_DEFAULT_APP_CHECK_TIME
 import com.duckduckgo.common.utils.MIN_VERSION
 import com.duckduckgo.common.utils.playstore.PlayStoreUtils
 import com.duckduckgo.data.store.api.SharedPreferencesProvider
@@ -137,6 +142,9 @@ open class BrowserActivity : DuckDuckGoActivity() {
 
     @Inject
     lateinit var kahfImageBlockedDao: KahfImageBlockedDao
+
+    @Inject
+    lateinit var analyticsService: AnalyticsService
 
     @Inject
     lateinit var spProvider: SharedPreferencesProvider
@@ -208,7 +216,25 @@ open class BrowserActivity : DuckDuckGoActivity() {
         observeKeyboardVisibility()
 
         checkForAppUpdate()
+        checkIfAppIsStillDefault()
     }
+
+    private fun checkIfAppIsStillDefault() {
+        val lastCheckTime = spProvider.getKahfSharedPreferences().getLong(LAST_DEFAULT_APP_CHECK_TIME, 0L)
+        if (lastCheckTime == 0L) spProvider.getKahfSharedPreferences().edit {
+            putLong(LAST_DEFAULT_APP_CHECK_TIME, System.currentTimeMillis())
+        }
+        if (System.currentTimeMillis() - lastCheckTime > 7.Days() ) {
+            val isDefault = isMyBrowserDefault(packageManager = packageManager, myPackageName = packageName)
+            if (isDefault) {
+                analyticsService.logEvent(AnalyticsEvent.DefaultBrowserCheck)
+                spProvider.getKahfSharedPreferences().edit {
+                    putLong(LAST_DEFAULT_APP_CHECK_TIME, System.currentTimeMillis())
+                }
+            }
+        }
+    }
+
 
     private fun checkForAppUpdate() {
         FirebaseRemoteConfig.getInstance().let { rc ->
